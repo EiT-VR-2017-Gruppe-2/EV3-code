45d67c5683d02a52f973c80cca8fff925b172b0c
Fredrik Bakken <fredda10x@gmail.com>
2017-02-22 20:42:26 +0100

    Updated IPv4 Validation
    Checking if the user input is a valid IPv4-address.

--- client.py
+++ client.py
@@ -3,10 +3,29 @@
 
 import socket
 
+# Validate if input has the structure of an IPv4-address
+def validateIPv4(input):
+    split = input.split('.')
+    if len(split) != 4:
+        return False
+    for x in split:
+        if not x.isdigit():
+            return False
+        i = int(x)
+        if i < 0  or i > 255:
+            return False
+    return True
+
 s = socket.socket()
 
 #TODO Check validity of input (?)
-host = input('Enter the EV3\'s IP address: ')
+while True:
+    host = raw_input("Enter the EV3\'s IP address: ")       # TODO: Check if raw_input works for the EV3
+    if not validateIPv4(host):
+        print("Invalid IPv4-address, please try again.")
+    else:
+        break
+
 port = 1234
 
 s.connect((host, port))
