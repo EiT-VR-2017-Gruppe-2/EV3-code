39cab0187d4ccc610b4e58a349909e75c82060fc
Anders Marstein Kruke <Anders Marstein Kruke>
2017-02-22 15:52:41 +0100

    Server can receive and execute some commands
    Commands includes opening and closing the claw, and using text-to-voice functionality

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import ev3dev.ev3 as ev3
+import ev3dev.fonts as fonts
 import time
 import socket
 
@@ -12,8 +13,24 @@ s.bind((host, port))
 
 s.listen(5)
 
+m = ev3.MediumMotor('outA')
+#m.run_timed(time_sp=3000, speed_sp=500)
+
+
 while True:
     c, addr = s.accept()
     print('Got connection from', addr)
     c.send(('trololo').encode('utf-8'))
+    while True:
+        msg = (c.recv(1024)).decode('utf-8')
+        if msg == '':
+            break
+        elif msg == 'open':
+            m.run_timed(time_sp=3000, speed_sp=-500)
+        elif msg == 'close':
+            m.run_timed(time_sp=3000, speed_sp=500)
+        else:
+            ev3.Sound.speak(msg).wait()
     c.close()
+
+
