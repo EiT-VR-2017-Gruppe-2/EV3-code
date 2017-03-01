04c4411e34b7ffbb7780680b3d77e1dd6cd2238f
Anders Marstein Kruke <anders.kruke@gmail.com>
2017-03-01 12:58:08 +0100

    Control the robot with a keyboard

--- server.py
+++ server.py
@@ -13,7 +13,10 @@ s.bind((host, port))
 
 s.listen(5)
 
-m = ev3.MediumMotor('outA')
+claw = ev3.MediumMotor('outA')
+left_motor = ev3.LargeMotor('outB')
+right_motor = ev3.LargeMotor('outC')
+
 #m.run_timed(time_sp=3000, speed_sp=500)
 
 
@@ -25,10 +28,25 @@ while True:
         msg = (c.recv(1024)).decode('utf-8')
         if msg == '':
             break
+        elif msg == 'fwd':
+            left_motor.run_forever(speed_sp=500)
+            right_motor.run_forever(speed_sp=500)
+        elif msg == 'bwd':
+            left_motor.run_forever(speed_sp=-500)
+            right_motor.run_forever(speed_sp=-500)
+        elif msg == 'left':
+            left_motor.run_forever(speed_sp=-500)
+            right_motor.run_forever(speed_sp=500)
+        elif msg == 'right':
+            left_motor.run_forever(speed_sp=500)
+            right_motor.run_forever(speed_sp=-500)
+        elif msg == 'stop':
+            left_motor.stop()
+            right_motor.stop()
         elif msg == 'open':
-            m.run_timed(time_sp=3000, speed_sp=-500)
+            claw.run_timed(time_sp=3000, speed_sp=-500)
         elif msg == 'close':
-            m.run_timed(time_sp=3000, speed_sp=500)
+            claw.run_timed(time_sp=3000, speed_sp=500)
         else:
             ev3.Sound.speak(msg).wait()
     c.close()
