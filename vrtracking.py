3d78b2b810370a5f55fa3a3c975a97a7a9dd9bc6
Anders Marstein Kruke <andersmk@ntnu.no>
2017-03-22 11:31:50 +0100

    Calculate controller-headset distance

--- vrtracking.py
+++ vrtracking.py
@@ -1,6 +1,7 @@
 import sys
 import time
 import openvr
+import math
 
 def GetYawValue(yaw1, yaw2):
     if (yaw1 >= 0 and yaw2 >= 0):
@@ -19,6 +20,10 @@ def GetOffsetYawValue(value, offset):
     elif v > 1:
         v = v-2
     return v
+
+def GetDistance(x1, y1, x2, y2):
+    return math.sqrt((x1-x2)**2+(y1-y2)**2)
+    
         
 
 openvr.init(openvr.VRApplication_Scene)
@@ -34,16 +39,30 @@ offset = GetYawValue(yaw1, yaw2)
 while True:
     openvr.VRCompositor().waitGetPoses(poses, len(poses), None, 0)
     hmd_pose = poses[openvr.k_unTrackedDeviceIndex_Hmd]
-    #print(hmd_pose.mDeviceToAbsoluteTracking)
-    #print((poses[0].mDeviceToAbsoluteTracking)[2][0])
-    #print((poses[0].mDeviceToAbsoluteTracking)[2][2])
-    #print('###################')
-    yaw1 = (poses[0].mDeviceToAbsoluteTracking)[2][0]
-    yaw2 = (poses[0].mDeviceToAbsoluteTracking)[2][2]
+    headset_tracking = poses[0].mDeviceToAbsoluteTracking
+    controller1_tracking = poses[3].mDeviceToAbsoluteTracking
+    controller2_tracking = poses[2].mDeviceToAbsoluteTracking
+    yaw1 = headset_tracking[2][0]
+    yaw2 = headset_tracking[2][2]
     yaw_value = GetYawValue(yaw1,yaw2)
-    print(yaw_value)
-    print(GetOffsetYawValue(yaw_value, offset))
-    print('')                               
+    #print(yaw_value)
+    #print(GetOffsetYawValue(yaw_value, offset))
+    #print('')
+
+    headset_x = headset_tracking[2][3]
+    headset_y = headset_tracking[0][3]
+
+    controller1_x = controller1_tracking[2][3]
+    controller1_y = controller1_tracking[0][3]
+
+    controller2_x = controller2_tracking[2][3]
+    controller2_y = controller2_tracking[0][3]
+
+    distance1 = GetDistance(headset_x, headset_y, controller1_x, controller1_y)
+    distance2 = GetDistance(headset_x, headset_y, controller2_x, controller2_y)
+
+    print(distance2)
+    
     sys.stdout.flush()
     time.sleep(0.2)
 
@@ -52,9 +71,9 @@ openvr.shutdown()
 '''
 poses[0] = Headset
 poses[1] = basestation??
-poses[2] = ???
+poses[2] = controller 2
 poses[3] = controller 1
-poses[4] = controller 2
+poses[4] = ???
 
 [[?,   ?,   ?, Y],
  [?,   ?,   ?, Z],
